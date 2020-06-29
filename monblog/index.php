8c254f1b09f3f39e78c6e5c12fba4dd7c7889283
digitaliX <studentdbx@gmail.com>
2020-06-29 19:15:38 +0200

    return to initial status

--- monblog/index.php
+++ monblog/index.php
@@ -1,6 +1,3 @@
-    <h1>Bienvenue sur mon Super Blog !</h1>  <br> 
-    <p>Derniers billets du blog :</p>
-    
     <?php
     include('db_connect.php');
     $post_perPage = 4;
@@ -8,7 +5,7 @@
     $post_numberReq = $db->query('SELECT id FROM posts');
     $post_number = $post_numberReq->rowCount();
     $page_number = ceil($post_number/$post_perPage);
-
+    
     if (isset($_GET['page']) && !empty($_GET['page']) && $_GET['page'] >0 && $_GET['page'] <= $page_number)
     {
         $current_page = $_GET['page'];
@@ -19,12 +16,24 @@
         $current_page = 1;
     }
     $start_entry = ($current_page-1)*$post_perPage;
-        //On récupère les cinqs derniers billets
-    $req = $db->query('SELECT id, title, introduction, content, DATE_FORMAT(creation_date, \'%e/%m/%Y à %Hh%imin%ss\') AS creation_date FROM posts ORDER BY id DESC LIMIT '.$start_entry.','.$post_perPage);
-
-    while ($data = $req->fetch())
-    {
+    //On récupère les cinqs derniers billets
+    $req = $db->query('SELECT id, title, introduction, content, DATE_FORMAT(creation_date, \'%e/%m/%Y à %Hh%imin%ss\') AS creation_date FROM posts ORDER BY creation_date DESC LIMIT '.$start_entry.','.$post_perPage);
+    
     ?>
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+        <h1>Bienvenue sur mon Super Blog !</h1>  <br> 
+        <p>Derniers billets du blog :</p>
+        <?php
+        while ($data = $req->fetch())
+         {
+        ?>
         <div class="news"> <!--- On affiche les cinq derniers billets. -->
             <?php include('post_view.php');?>
             <br>
@@ -33,23 +42,25 @@
                 </em>
             </p>  
         </div>          
-    <?php
-    } // Fin de la boucle des billets.
-    $req->closeCursor(); //On libère le curseur pour la prochaine requête.
-    //On fait une boucle pour écrire les liens vers chacune des pages.
-    echo 'Page :';
-    for ($i=1; $i <= $page_number; $i++)
-    { 
-        if ($i == $current_page)
-        {
-            echo $i. ' '. '/';
+        <?php
+        } // Fin de la boucle des billets.
+        $req->closeCursor(); //On libère le curseur pour la prochaine requête.
+        //On fait une boucle pour écrire les liens vers chacune des pages.
+        echo 'Page :';
+        for ($i=1; $i <= $page_number; $i++)
+        { 
+            if ($i == $current_page)
+            {
+                echo $i. ' '. '/';
+            }
+            else
+            {
+                echo ' <a href="index.php?page='.$i.'">'.$i.'</a> /';
+            }
         }
-        else
-        {
-            echo ' <a href="index.php?page='.$i.'">'.$i.'</a> /';
-        }
-    }
-?>
+        ?>        
+        </body>
+    </html>
 
 
 
